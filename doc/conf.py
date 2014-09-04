202c77da85d63045913522a3bb05e3ce97e5e75d
Jan Safranek <jsafrane@redhat.com>
2014-09-04 11:23:02 +0200

    added path to theme

--- doc/conf.py
+++ doc/conf.py
@@ -97,6 +97,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+print "My file:" , __file__
+import os
+path = os.path.dirname(__file__)
+print "My file:" , __file__, "path:", path
+sys.path.append(path)
+
 import my_rtd_theme
 html_theme = "my_rtd_theme"
 html_theme_path = [my_rtd_theme.get_html_theme_path()]
