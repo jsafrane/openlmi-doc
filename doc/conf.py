aa231f158ade09221f473b7387e96f0e1640c9ca
Jan Safranek <jsafrane@redhat.com>
2014-09-17 15:07:38 +0200

    added automodule

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ includeClasses = False
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.autodoc']
 
 
 # Add any paths that contain templates here, relative to this directory.
