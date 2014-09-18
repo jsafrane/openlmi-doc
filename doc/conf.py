38e8d1591ab02c01056735c50f1d63621cbf6b8f
Jan Safranek <jsafrane@redhat.com>
2014-09-18 14:23:28 +0200

    added python sources

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ includeClasses = False
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('python/usr/lib/python2.7/site-packages'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -30,7 +30,7 @@ includeClasses = False
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.ifconfig', 'sphinx.ext.autodoc', 'sphinx.ext.todo']
 
 
 # Add any paths that contain templates here, relative to this directory.
