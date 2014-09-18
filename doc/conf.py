6abbd7add96791ffef97d0abab5145111ba052f6
Jan Safranek <jsafrane@redhat.com>
2014-09-18 16:38:23 +0200

    fixed typo

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['ppywbem']
+MOCK_MODULES = ['pywbem']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
