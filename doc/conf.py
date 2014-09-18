e9eb44c87103f2fdebb64a5ff743ab1347be7729
Jan Safranek <jsafrane@redhat.com>
2014-09-18 15:07:02 +0200

    Added base python

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,8 @@ includeClasses = False
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('python/usr/lib/python2.7/site-packages'))
+sys.path.insert(0, os.path.abspath('python'))
+sys.path.insert(0, os.path.abspath('python/lmi'))
 
 # -- General configuration -----------------------------------------------------
 
